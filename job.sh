73a650f489542cf2d1a67f6e94b0d64222cf4a5a
Brad <bradley.j.baker.1982@gmail.com>
2016-10-08 10:22:15 -0400

    fixes

--- job.sh
+++ job.sh
@@ -1,6 +1,6 @@
 case "$(pidof ffmpeg | wc -w)" in
 
-0)  python /work/mmp4_automator/manaul.py -i /incoming -a
+0)  python /work/mp4_automator/manual.py -i /incoming -a
     ;;
 1)  echo "ffmpeg running"
     ;;
